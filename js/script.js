8212ce3e9b83ba6f457176cf32334b0be4404809
Clifford Thompson <clifford.thompson@gmail.com>
2020-04-17 23:30:14 -0700

    master: Added a statistics chart.

--- js/script.js
+++ js/script.js
@@ -18,8 +18,10 @@
 
 const FRAMES_PER_SECOND = 30;
 const FRAMES_PER_DAY = FRAMES_PER_SECOND;
+const CHART_START_DAYS = 100;
 let canvas, context;
 let balls = [];
+let statsChart;
 let currentDay = 0
 let currentDayFrame = 0;
 let recoveryInDays = 0;
@@ -48,6 +50,56 @@ function resetSimulation() {
   for(var i = 0; i < numberOfBalls; ++i) {
     balls.push(createBall());
   }
+
+  var chartData = {
+    labels: Array.from(Array(CHART_START_DAYS+1).keys()),
+    datasets: [
+      {
+        label: "Infected",
+        data: [],
+        borderColor: "red",
+        backgroundColor: 'transparent',
+      },
+      {
+        label: "Immune",
+        data: [],
+        borderColor: "lightgreen",
+        backgroundColor: 'transparent',
+      },
+      {
+        label: "Dead",
+        data: [],
+        borderColor: "black",
+        backgroundColor: 'transparent',
+      },
+      {
+        label: "Unaffected",
+        data: [],
+        borderColor: "lightblue",
+        backgroundColor: 'transparent',
+      },]
+  };
+
+  var dataGraphContext = document.getElementById("data_graph");
+  statsChart = new Chart(dataGraphContext, {
+    type: 'line',
+    data: chartData,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: true,
+      }
+    }
+  });
+
+  // Push all the starting data into the chart
+  updateChart(currentDay, balls);
 }
 
 function updateSimulation() {
@@ -60,7 +112,7 @@ function updateSimulation() {
 
   for(var i = 0; i < balls.length; ++i) {
     drawBall(balls[i]);
-  }
+  };
 }
 
 function drawBackground() {
@@ -132,6 +184,7 @@ function updateTime(balls) {
     currentDayFrame = 0;
     updateDeaths(balls);
     updateImmunity(balls);
+    updateChart(currentDay, balls);
   }
 }
 
@@ -201,4 +254,39 @@ function updateInfections(balls) {
       }
     }
   }
+}
+
+function updateChart(currentDay, balls) {
+
+  let totalInfected = 0;
+  let totalImmune = 0;
+  let totalDead = 0;
+  let totalUnaffected = 0;
+
+  for(var i = 0; i < balls.length; ++i) {
+    if(balls[i].infected){
+      totalInfected++;
+    }
+    else if(balls[i].immune){
+      totalImmune++;
+    }
+    else if(balls[i].dead){
+      totalDead++;
+    }
+    else {
+      totalUnaffected++;
+    }
+  }
+
+  // After a certain amount of time start adding values
+  // to the x-axis
+  if(currentDay > CHART_START_DAYS) {
+    statsChart.data.labels.push(currentDay);
+  }
+
+  statsChart.data.datasets[0].data.push(totalInfected);
+  statsChart.data.datasets[1].data.push(totalImmune);
+  statsChart.data.datasets[2].data.push(totalDead);
+  statsChart.data.datasets[3].data.push(totalUnaffected);
+  statsChart.update();
 }
